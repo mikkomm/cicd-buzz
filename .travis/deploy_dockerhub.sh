f3761dded75cc19addbc924e4e1498ee95894ae1
N7513 <n7513@student.jamk.fi>
2020-04-13 08:45:13 +0300

    Updated deployment script.

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -7,6 +7,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_PATH
+docker build -f Dockerfile -t $DOCKER_PATH:$TAG .
+#docker push $DOCKER_USER/$DOCKER_PATH:$TAG
 docker push $DOCKER_PATH
+
 
