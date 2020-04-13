84db94a42368e3976756c5552b9c49ef7529cc9f
N7513 <n7513@student.jamk.fi>
2020-04-13 08:33:31 +0300

    Updated deployment script.

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
+
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t cicd-buzz:$TAG .
-docker push $DOCKER_USER/cicd-buzz:$TAG
+docker build -f Dockerfile -t $DOCKER_PATH:$TAG .
+docker push $DOCKER_USER/$DOCKER_PATH:$TAG
 
